1e18357b8b1f89738b4ba2a7e54aa23c1e6ad5b3
Angelika222 <angelikabenavidez21@gmail.com>
2024-09-09 17:22:29 +0800

    Update mike

---
+++ mike/angelika/activity1.pp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    string firstName = "Angelika";
+    string middleName = "Napud";
+    string lastName = "Benavidez";
+   
+    string fullName = "Fullname: " + firstName + " " + middleName + " " + lastName + " BSCS-1";
+
+ 
+ 
+    cout << fullName << endl;
+
+    return 0;
+}
