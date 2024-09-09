526d00d66e25064069ebce101427f7eb4af6689c
Angelika222 <angelikabenavidez21@gmail.com>
2024-09-09 19:24:57 +0800

    Update angelikabenavidez

---
+++ angelikabenavidez/activity2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1 = 2;
+    int number2 = 8;
+
+    int add = number1 + number2;
+    int minus = number1 - number2;
+    int product = number1 * number2;
+    int division = number1 / number2;
+
+    cout << "Addition: " << add << endl;
+    cout << "Subtraction: " << minus << endl;
+    cout << "Multiplication: " << product << endl;
+    cout << "Division: " << division << endl;
+
+    return 0;
+}
